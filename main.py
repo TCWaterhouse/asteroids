2d1b94a1a90aa265751ee71f14c085a06e6ac08e
TCWaterhouse <thomas_waterhouse@hotmail.co.uk>
2024-09-23 15:01:15 +0100

    Deleting blank line

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
-        
-        dt = clock.tick(60) / 1000
 
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
